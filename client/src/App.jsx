3f399f2ea0125b7b8901ad5568ed2c82586b0407
Daze2Op <rawat.chinmaya@gmail.com>
2025-09-11 13:44:14 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter,  Route, Routes } from 'react-router-dom'
-import Layout from './Layout/layout'
+import Layout from './Layout/Layout'
 import Index from './pages/index' 
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
